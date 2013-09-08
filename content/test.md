2c5127cbd2c6426d617a49e0db5d65e1cc73e667
Dmytro Zikrach <zikrach.dm@gmail.com>
2013-09-08 16:33:01 +0300

    test post

---
+++ content/test.md
@@ -0,0 +1,7 @@
+Title: Свой блог на GitHub или Как приручить пеликана
+Date: 2013-09-08 16:30
+Category: Web
+Tags: Python, Blog
+Slug: pelican
+Author: dimon
+Summary: О том, как настроить свой блог
---
+++ local_settings.py
@@ -0,0 +1,11 @@
+#local_settings.py
+# -*- coding: utf-8 -*-
+
+AUTHOR = 'dimon'
+SITENAME = 'About All!!!'
+SITEURL = 'http://zzk.github.io'
+
+TIMEZONE = 'Europe/Kiev'
+
+LOCALE = 'ru_RU.UTF-8'
+DEFAULT_LANG = 'ru'
